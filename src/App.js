e303c5f2ac75692c2f8d0f629014c2f2ede8a626
Pradyu <pvasu009@uottawa.ca>
2023-06-26 17:17:06 -0400

    preliminary navbar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-import {Button, Alert, Breadcrumb} from 'react-bootstrap';
+import { Button, Alert, Breadcrumb } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavigationBar from './Navbar';
 import Games from './Games';
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <NavigationBar />
+        <Routes>
+          <Route path="/Games.js" element={<Games />} />
+          {/* Add more routes as needed */}
+        </Routes>
+      </Router>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Alert variant='primary'>Le button</Alert>
+        <Alert variant="primary">Le button</Alert>
         <Button>Test Button</Button>
         <p>
           Edit <code>src/App.js</code> and save to reload.
