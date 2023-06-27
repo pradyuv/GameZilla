1fff64393f4ea315d83607a92ac781da639a4b89
Pradyu <pvasu009@uottawa.ca>
2023-06-26 23:14:05 -0400

    added footer but need to fix card changing dimensions

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavigationBar from './Navbar';
 import Games from './Games';
+import Footer from './Footer';
 
 import logo1 from './images/firstcarousel.jpg';
 import logo2 from './images/secondcarousel.jpg';
@@ -11,7 +12,7 @@ import logo3 from './images/thirdcarousel.jpg';
 
 function App() {
   return (
-    <div className="App dark-background">
+    <div className="App bg-dark">
       <Router>
         <NavigationBar />
         <Routes>
@@ -21,28 +22,30 @@ function App() {
       </Router>
       <Row>
         <Col>
-          <Card className="h-100 dark-background">
+          <Card className="h-100 bg-dark">
             <Card.Body className="d-flex flex-column align-items-center">
-              <Carousel>
-                <Carousel.Item>
-                  <img className="d-block w-100" src={logo1} alt="Slide 1" />
-                  <Carousel.Caption>
-                    <h3>Diablo 4</h3>
-                  </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                  <img className="d-block w-100" src={logo2} alt="Slide 2" />
-                  <Carousel.Caption>
-                    <h3>Destiny 2: Lightfall</h3>
-                  </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                  <img className="d-block w-100" src={logo3} alt="Slide 3" />
-                  <Carousel.Caption>
-                    <h3>Elden Ring</h3>
-                  </Carousel.Caption>
-                </Carousel.Item>
-              </Carousel>
+              <div className="carousel-container">
+                <Carousel>
+                  <Carousel.Item>
+                    <img className="d-block w-100" src={logo1} alt="Slide 1" />
+                    <Carousel.Caption>
+                      <h3>Diablo 4</h3>
+                    </Carousel.Caption>
+                  </Carousel.Item>
+                  <Carousel.Item>
+                    <img className="d-block w-100" src={logo2} alt="Slide 2" />
+                    <Carousel.Caption>
+                      <h3>Destiny 2: Lightfall</h3>
+                    </Carousel.Caption>
+                  </Carousel.Item>
+                  <Carousel.Item>
+                    <img className="d-block w-100" src={logo3} alt="Slide 3" />
+                    <Carousel.Caption>
+                      <h3>Elden Ring</h3>
+                    </Carousel.Caption>
+                  </Carousel.Item>
+                </Carousel>
+              </div>
               <div className="text-center">
                 <Alert variant="primary">Le button</Alert>
                 <Button>Test Button</Button>
@@ -54,7 +57,7 @@ function App() {
           </Card>
         </Col>
         <Col>
-          <Card className="h-100 dark-background">
+          <Card className="h-100 bg-dark">
             <Card.Body>
               <Card.Text className="p-4">
                 Welcome to our website! We aim to provide you with the best gaming experience. Explore our collection of games and discover new adventures. Have fun and enjoy your stay!
@@ -63,6 +66,7 @@ function App() {
           </Card>
         </Col>
       </Row>
+      <Footer/>
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer bg-black">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-12 text-center">
+            <p className="text-white">GameZilla is a great video game store.</p>
+            <p className="text-white">© 2023 GameZilla. All rights reserved.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
