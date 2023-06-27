1102e420994a7bc0db9900733ed03bb13ac9f721
Pradyu <pvasu009@uottawa.ca>
2023-06-26 22:26:29 -0400

    implemented preliminary carousel

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-import { Button, Alert, Breadcrumb, Row, Col } from 'react-bootstrap';
+import React from 'react';
+import { Button, Alert, Breadcrumb, Row, Col, Carousel, Card } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavigationBar from './Navbar';
 import Games from './Games';
 
+import logo1 from './images/firstcarousel.jpg';
+import logo2 from './images/secondcarousel.jpg';
+import logo3 from './images/thirdcarousel.jpg';
+
 function App() {
   return (
-    <div className="App">
+    <div className="App dark-background">
       <Router>
         <NavigationBar />
         <Routes>
@@ -17,25 +20,36 @@ function App() {
         </Routes>
       </Router>
       <Row>
-        <Col className="dark-background">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <Alert variant="primary">Le button</Alert>
-            <Button>Test Button</Button>
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-          </header>
+        <Col>
+          <Card className="h-100 dark-background">
+            <Card.Body className="d-flex flex-column align-items-center">
+              <Carousel>
+                <Carousel.Item>
+                  <img className="d-block w-100" src={logo1} alt="Slide 1" />
+                </Carousel.Item>
+                <Carousel.Item>
+                  <img className="d-block w-100" src={logo2} alt="Slide 2" />
+                </Carousel.Item>
+                <Carousel.Item>
+                  <img className="d-block w-100" src={logo3} alt="Slide 3" />
+                </Carousel.Item>
+              </Carousel>
+              <div className="text-center">
+                <Alert variant="primary">Le button</Alert>
+                <Button>Test Button</Button>
+                <p>
+                  Edit <code>src/App.js</code> and save to reload.
+                </p>
+              </div>
+            </Card.Body>
+          </Card>
         </Col>
-        <Col className="dark-background">
-        <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <Alert variant="primary">Le button</Alert>
-            <Button>Test Button</Button>
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-          </header>
+        <Col>
+          <Card className="h-100 dark-background">
+            <Card.Body>
+              {/* Content for the second column */}
+            </Card.Body>
+          </Card>
         </Col>
       </Row>
     </div>
