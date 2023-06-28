0d0cf594d6045f35355da792b5e7f9c72f469f14
Pradyu <pvasu009@uottawa.ca>
2023-06-28 17:14:05 -0400

    workign reload but no alert yet

--- src/ThisGame.js
+++ src/ThisGame.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Container, Row, Col, Card, Button, Form } from 'react-bootstrap';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import NavigationBar from './Navbar';
 import Footer from './Footer';
 import { FaShoppingCart } from 'react-icons/fa';
@@ -8,6 +8,7 @@ import gamesData from './games.json';
 
 function ThisGame() {
   const location = useLocation();
+  const navigate = useNavigate();
   const { name, imagePath } = location.state || {};
 
   const [gameInfo, setGameInfo] = useState(null);
@@ -15,9 +16,9 @@ function ThisGame() {
   useEffect(() => {
     if (!name || !imagePath) {
       // Handle the case when location.state is null or missing properties
-      alert("Game information not sent properly!");
       console.log('name:', name);
       console.log('imagePath:', imagePath);
+      alert("Game information not passed correctly");
     } else {
       const game = gamesData.games.find(game => game.name === name);
       if (game) {
@@ -29,6 +30,12 @@ function ThisGame() {
   }, [name, imagePath]);
 
   const { description, price, imagePath: gameImagePath } = gameInfo || {};
+
+  const handleReviewSubmit = (event) => {
+    event.preventDefault();
+    // Perform review submission logic
+    navigate('/');
+  };
 
   return (
     <div className="bg-dark">
@@ -57,7 +64,7 @@ function ThisGame() {
                 <Button variant="success" className='mt-3' style={{ padding: '8px', width: '40px', height: '40px' }}>
                   <FaShoppingCart size={20} />
                 </Button>
-                <Form className="mt-4">
+                <Form className="mt-4" onSubmit={handleReviewSubmit}>
                   <Form.Group controlId="reviewForm">
                     <Form.Label>Leave a review:</Form.Label>
                     <Form.Control as="textarea" rows={3} placeholder="Write your review here" />
